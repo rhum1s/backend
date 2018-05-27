69d4b4603af87478fe135a438cc9bb588a97a1bb
rhum1s <romain.souweine@hotmail.fr>
2018-05-27 15:05:57 +0200

    nginx debug config

--- nginx_install.sh
+++ nginx_install.sh
@@ -1,12 +1,17 @@
 # Install nginx latest official repository version and extras package
 # Testé avec Ubuntu 16.04.4 LTS
 # FIXME: Package depends on ubuntu versions
+# Files will go in /mnt/sdb/nginx_data folder. Must exist.
 
 # Exit on error
 set -e
 
-# Going home
-cd ~
+# Must stay in backend dir to access files
+# cd ~
+
+# TODO Check /mnt/sdb/nginx_data folder
+
+# TODO: Ask for [USER]
 
 echo "- Adding nginx repository ..."
 sudo touch /etc/apt/sources.list.d/nginx.list
@@ -48,99 +53,19 @@ sudo service php7.0-fpm restart
 echo "... done."
 
 echo "- Create main page ..."
-sudo touch /var/www/index.php
-echo "<?php echo '*';?>" | sudo tee /var/www/index.php
+sudo touch /mnt/sdb/nginx_data/index.php
+echo "<?php echo 'Working';?>" | sudo tee /mnt/sdb/nginx_data/index.php
 echo "... done."
 
+sudo rm /etc/nginx/sites-enabled/default
+sudo cp data/sites_enabled_main /etc/nginx/sites-enabled/sites_enabled_main
+sudo chown -R [USER]:root /etc/nginx/sites-enabled/sites_enabled_main
+
+# User owner
+# FIXME: Ask for user
+sudo chown -R [USER]:root /mnt/sdb/nginx_data
+
 echo "- Creating default config file ..."
-echo """
-# NOTES: 
-# Si alias, il faut utiliser $request_filename et pas $fastcgi_script_name pour script_filename.
-
-user www-data;
-worker_processes  2; # same as number of cores
-
-events {
-    worker_connections  1024;
-}
-
-http {
-    include       mime.types;
-    default_type  text/html;
-    sendfile        on;
-    keepalive_timeout  65;
-   
-	ssl_protocols TLSv1 TLSv1.1 TLSv1.2; # Dropping SSLv3, ref: POODLE
-	ssl_prefer_server_ciphers on;
-
-	gzip on;
-	gzip_disable 'msie6';
-	
-    # -- Localhost
-    server {
-	
-        listen       80;
-        server_name  localhost;
-        
-        # -- Main
-        root /var/www;
-        index index.php;              
-
-        # location / {
-            # auth_basic 'Login admin';
-            # auth_basic_user_file /opt/nginx/logins/.htpasswd;
-        # }
-        
-		location ~ \.php$ { 
-			try_files $uri =404;  #If a file isn’t found, 404
-			include /etc/nginx/fastcgi.conf; # Include Nginx’s fastcgi configuration
-			fastcgi_pass unix:/run/php/php7.0-fpm.sock; # Look for the FastCGI Process Manager at this location 
-		} 		
-        
-       
-
-        # # -- Notebooks Jupyter
-        # location /notebooks {
-            # alias  /home/airpaca/web/notebooks;
-            # # index  index.php;
-
-            # auth_basic 'Login';
-            # auth_basic_user_file /opt/nginx/.htpasswd.master;  
-            
-            # location ~ \.php$ {             
-                # fastcgi_pass   127.0.0.1:9000;
-                # fastcgi_index  index.php;
-                # fastcgi_param  SCRIPT_FILENAME $request_filename;  
-                # include        fastcgi_params;             
-            # }  
-                    
-        # }          
-        
-        
-    }
-}
-
-# mail {
-	# # See sample authentication script at:
-	# # http://wiki.nginx.org/ImapAuthenticateWithApachePhpScript
-
-	# # auth_http localhost/auth.php;
-	# # pop3_capabilities 'TOP' 'USER';
-	# # imap_capabilities 'IMAP4rev1' 'UIDPLUS';
-
-	# server {
-		# listen     localhost:110;
-		# protocol   pop3;
-		# proxy      on;
-	# }
-
-	# server {
-		# listen     localhost:143;
-		# protocol   imap;
-		# proxy      on;
-	# }
-# }
-""" | sudo tee /etc/nginx/nginx.conf
 sudo nginx -t
 echo "... done."
 
