4033968119989d12e302b152dc31e888613f961f
rhum1s <romain.souweine@hotmail.fr>
2018-06-06 21:59:36 +0200

    nginx - Composer, backup - maj

--- backup.sh
+++ backup.sh
@@ -9,6 +9,7 @@
 # 	export VISUAL=vi
 # 	crontab -e
 # 	0 3 * * * bash /toto/sdb/backup.sh
+# Fixme: Mail server name
 
 # --- Configuration
 to_save=( 
@@ -24,9 +25,18 @@ receiver_mail="toto@mail.com"
 the_host=`hostname -f`
 set -e
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-trap 'echo "\"${last_command}\" command filed with exit code $?."  | mail -s "${the_host} - Backup ERROR" -a From:tot\<backup@vps540666.ovh.net\> ${receiver_mail}' EXIT # echo an error message before exiting
+trap 'if [[ $? != 0 ]]; then echo "\"${last_command}\" command filed with exit code $?."  | mail -s "${the_host} - Backup ERROR" -a From:tot\<backup@vps540666.ovh.net\> ${receiver_mail}; fi' EXIT # echo an error message before exiting
 
+# Echo date to be put in log
+the_date=`date '+%Y-%m-%d %H:%M:%S'`
+echo "Starting $the_date"
+
+# Sync everything
 for l in "${to_save[@]}"
 do
 	rsync -v -a $l $backup_lgn@$backup_host:$backup_dir
 done
+
+# Echo date to be put in log
+the_date=`date '+%Y-%m-%d %H:%M:%S'`
+echo "Ending $the_date"
