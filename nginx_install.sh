39e6eca262225243f4f1813b8d8334a412a5f14d
rhum1s <romain.souweine@hotmail.fr>
2018-06-09 19:43:58 +0200

    nginx - php7.0-pgsql

--- nginx_install.sh
+++ nginx_install.sh
@@ -39,6 +39,7 @@ echo "- Installing php7-fpm ..."
 sudo apt-get install php7.0 php7.0-fpm
 sudo apt-get install php7.0-curl
 sudo apt-get install php7.0-mbstring
+sudo apt-get install php7.0-pgsql
 sudo systemctl restart php7.0-fpm
 sudo systemctl status php7.0-fpm
 sudo systemctl restart nginx
